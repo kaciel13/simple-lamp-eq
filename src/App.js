5018ee9239bb32368450290c71817863a294b444
kaciel13 <kaciel13@yahoo.com>
2025-02-27 22:56:04 +0600

    Add simple-lamp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
+import lamp1 from './images/lamp1.png'; // Убедитесь, что путь правильный
+import lamp2 from './images/lamp2.png'; // Убедитесь, что путь правильный
+import lamp3 from './images/lamp3.png'; // Убедитесь, что путь правильный
 
 function App() {
   return (
     <div className="App">
-       <h1>Hello world</h1>
+      <div className="lamp">
+        <img src={lamp1} alt="Lamp 1" className="lamp-image" />
+        <img src={lamp2} alt="Lamp 2" className="lamp-image" />
+        <img src={lamp3} alt="Lamp 3" className="lamp-image" />
+      </div>
     </div>
   );
 }
+
 
 export default App;
